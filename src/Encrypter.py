d8ffe916ee34d284f87878c653b9098fec84cb54
Tejaswini3002 <98581866+Tejaswini3002@users.noreply.github.com>
2022-04-12 12:18:36 +0530

    Update Encrypter.py

--- src/Encrypter.py
+++ src/Encrypter.py
@@ -1,1 +1,16 @@
 
+import base64
+import hashlib
+from PIL import Image
+from random import randint
+from src.AESCipher import AESCipher
+
+class Encrypter:
+    def init(self, text,key):
+        self.text = text
+        self.key =  key
+        
+    def encrypt_image(self):
+        aes = AESCipher(self.key)
+        cipher = aes.encrypt(self.text)
+        return cipher
