536c3eabd17114cc7ae3ad84086de04572821fb5
Tejaswini3002 <98581866+Tejaswini3002@users.noreply.github.com>
2022-04-12 12:17:54 +0530

    Update Decrypter.py

--- src/Decrypter.py
+++ src/Decrypter.py
@@ -1,1 +1,20 @@
 
+import base64
+import hashlib
+from PIL import Image
+from random import randint
+from src.AESCipher import AESCipher
+
+class Decrypter:
+    def init(self, cipher):
+        self.cipher = cipher
+        
+    def decrypt_image(self,k):
+        key = k
+        cipher = self.cipher
+        aes = AESCipher(key)
+        base64_decoded = aes.decrypt(cipher)
+        fh = open("decryptedImage.png", "wb")
+        fh.write(base64.b64decode(base64_decoded))
+        fh.close() 
+        return (base64.b64decode(base64_decoded))
